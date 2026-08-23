da8303df2ca2ecf73bb1d229e0d8dc0f02720028
Arlando <arlandorudolf@gmail.com>
2026-08-23 12:28:55 +0700

    add sonolus-server-web

--- server.js
+++ server.js
@@ -3,9 +3,11 @@ import { Sonolus } from '@sonolus/express'
 
 const app = express()
 const sonolus = new Sonolus()
-
 sonolus.load('./pack')
 
+const sonolusShare = new SonolusSpaShare('./public')
+
 app.use(sonolus.router)
+app.use(sonolusShare.router)
 
 export default app
