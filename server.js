7f5bf37732dbfe60e72c2eaeb76bdc36ab5ea3a0
Arlando <arlandorudolf@gmail.com>
2026-08-23 12:20:01 +0700

    fix server.js and vercel config for deploy

--- server.js
+++ server.js
@@ -4,10 +4,8 @@ import { Sonolus } from '@sonolus/express'
 const app = express()
 const sonolus = new Sonolus()
 
-sonolus.load('./pack')  // atau './pack' kalau nggak di-rename ke 'source'
+sonolus.load('./pack')
 
 app.use('/sonolus', sonolus.router)
 
-app.listen(8000, () => {
-    console.log('Server jalan di port 8000')
-})
+export default app
